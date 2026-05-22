c8b08b3702e37c75fa900467bf62639c1cbac2b1
MATHEUS HENRIQUE DE CAMPOS RUMAO <1051392421015@fatec.corp>
2026-05-22 11:33:54 -0300

    Melhora interface da home e tela de carnes

--- app/src/main/java/com/example/carnecertamobile/CarnesActivity.java
+++ app/src/main/java/com/example/carnecertamobile/CarnesActivity.java
@@ -2,69 +2,23 @@ package com.example.carnecertamobile;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CarnesActivity extends AppCompatActivity {
 
-    TextView txtLista;
-    EditText edtCarne;
-
-    Button btnAdicionar;
-    Button btnLimpar;
-
-    DataBaseHelper banco;
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carnes);
 
-        txtLista = findViewById(R.id.txtLista);
+        btnVoltar = findViewById(R.id.btnVoltar);
 
-        edtCarne = findViewById(R.id.edtCarne);
-
-        btnAdicionar = findViewById(R.id.btnAdicionar);
-
-        btnLimpar = findViewById(R.id.btnLimpar);
-
-        banco = new DataBaseHelper(this);
-
-        atualizarLista();
-
-        btnAdicionar.setOnClickListener(v -> {
-
-            String nomeCarne = edtCarne.getText().toString();
-
-            if(!nomeCarne.isEmpty()) {
-
-                banco.inserirCarne(nomeCarne);
-
-                edtCarne.setText("");
-
-                atualizarLista();
-
-            }
-
+        btnVoltar.setOnClickListener(v -> {
+            finish();
         });
-
-        btnLimpar.setOnClickListener(v -> {
-
-            banco.limparCarnes();
-
-            txtLista.setText("");
-
-        });
-
-    }
-
-    private void atualizarLista() {
-
-        String carnes = banco.listarCarnes();
-
-        txtLista.setText(carnes);
 
     }
 }
